a923bf733dba8f3ccf49a404b87d17b8f9825828
Albert <90168343+albertryanlamar@users.noreply.github.com>
2025-10-18 14:11:13 +0800

    Add files via upload

---
+++ nAmax/selenium2025/src/test/java/features/selfstudy/day31/BootStrapDropdown.java
@@ -0,0 +1,22 @@
+package features.selfstudy.day31;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BootStrapDropdown {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		
+	}
+
+}
